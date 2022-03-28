3dd6e38a62726b8e311405648696991f69e40082
0cmat <mattcon022@gmail.com>
2022-03-28 10:40:35 -0300

    Iniciado a implementação do Slider

--- Paixão Cakes's/js/init.js
+++ Paixão Cakes's/js/init.js
@@ -1,7 +1,17 @@
+
+
 (function($){
   $(function(){
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.carousel');
+      var instances = M.Carousel.init(elems, options);
+    });
 
     $('.sidenav').sidenav();
 
-  }); // end of document ready
+  }); // end of document read
+
+  $(document).ready(function(){ jQuery
+    $('.carousel').carousel();
+  });
 })(jQuery); // end of jQuery name space
